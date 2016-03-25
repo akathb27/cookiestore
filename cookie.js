ab0c8a3f3613d47b42e1617a75156782c77b3e97
Abby Burks <akathb27@gmail.com>
2016-03-24 18:47:54 -0700

    adding index file

---
+++ cookie.js
@@ -0,0 +1,65 @@
+var pioneerSquare = {
+  minNumber: 17,
+  maxNumber: 88,
+  avgCookie: 5.2
+};
+pioneerSquare.getCookietotal = function() {
+  return math.random() this.minNumber =< this.maxNumber;
+}
+
+
+var portlandAirport = {
+  minNumber: 6,
+  maxNumber: 24,
+  avgCookie: 1.2
+};
+portlandAirport.getCookietotal = function() {
+  return
+}
+
+var washSquare = {
+  minNumber: 11,
+  maxNumber: 38,
+  avgCookie: 1.9
+};
+washSquare.getCookietotal = function() {
+  return
+}
+
+var sellWood = {
+  minNumber: 20,
+  maxNumber: 48,
+  avgCookie: 3.3
+};
+sellWood.getCookietotal = function() {
+  return
+}
+
+var pearlDis = {
+  minNumber: 3,
+  maxNumber: 24,
+  avgCookie: 2.6
+};
+pearlDis.getCookietotal = function() {
+  return
+}
+
+
+
+//function addTotals(){
+  //var storeList = document.getElementById("pioneerSquare");
+  //listItem.innerHTML = pioneerSquare.getCookietotal();
+  //var storeList = document.getElementById("portlandAirport");
+  ///listItem.innerHTML = portlandAirport.getCookietotal();
+  //var storeList = document.getElementById("washSquare");
+//  listItem.innerHTML = washSquare.getCookietotal();
+//  var storeList = document.getElementById("sellWood");
+//  listItem.innerHTML = sellWood.getCookietotal();
+//  var storeList = document.getElementById("pearlDis");
+//  listItem.innerHTML = pearlDis.getCookietotal();
+//}
+
+
+//function addtoTable (){
+  //var table = document.getElementById("storeList")
+//}
